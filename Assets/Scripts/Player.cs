89d6c8c3fa99ceaae8713575f824bc187588bd27
three-halves <threehalves1@gmail.com>
2023-09-13 10:05:45 -0500

    jumping on cubes fix

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -209,8 +209,9 @@ public class Player : MonoBehaviour
 
     public bool Grounded()
     {
-        return Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0f, Vector2.down, 0.1f, groundLayer);
+        RaycastHit2D hit = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0f, Vector2.down, 0.1f, groundLayer);
+        if (hit == null || hit.collider == null) return false;
+        if (hit.collider.gameObject.GetComponent<Cube>() == null) return true;
+        return (hit.collider.gameObject.GetComponent<Cube>().teamAlignment == teamAlignment);
     }
 }
-
-
